b39158003de3f1ea723cd014d69a08ccf2d2ffb0
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-09-28 13:20:26 +0200

    Create verifyToken middleware

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 const config = require('../config');
+const verifyToken = require('./verifyToken');
 
 const router = Router();
 
@@ -45,20 +46,14 @@ router.post(
 
 router.get(
     '/me',
+    verifyToken,
     async (req, res, next) => {
         console.log('Dashboard');
-        const token = req.headers['x-access-token'];
-        if (!token) {
-            return res.status(401).json( { auth: false, message: 'No token provided'} );
+        const user = await User.findById(req.userId, { password: 0, __v: 0 } );
+        if (!user) {
+            return res.status(404).json( {message: 'No user found'} );
         } else {
-            const decoded = jwt.verify(token, config.secret);
-            console.log(decoded);
-            const user = await User.findById(decoded.id, { password: 0, __v: 0 } );
-            if (!user) {
-                return res.status(404).json( {message: 'No user found'} );
-            } else {
-                return res.status(200).json( {message: 'Dashboard', user: decoded.id, user} );
-            }
+            return res.status(200).json( {message: 'Dashboard', user} );
         }
     }
 );
