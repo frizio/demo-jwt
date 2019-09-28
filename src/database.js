518a946b861ec9a8447c463e651470edb7c1f8a7
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-09-28 06:30:14 +0200

    Set db connection

--- src/database.js
+++ src/database.js
@@ -1,1 +1,14 @@
 // Database connection
+const mongoose = require('mongoose');
+
+const DB_URL = 'mongodb://app_user:password@localhost:27017/demo';
+
+mongoose.connect(
+    DB_URL,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+).then(
+    console.log('Database connection successfully')
+);
