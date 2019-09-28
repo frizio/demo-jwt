41ce0cda7e29b9347a07b18dd22f4f21805cc850
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-09-28 06:36:37 +0200

    Create User model

--- src/models/User.js
+++ src/models/User.js
@@ -1,1 +1,13 @@
 // Model
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: String,
+        email: String,
+        password: String
+    }
+);
+
+// Create collection
+module.exports = model('User', userSchema);
